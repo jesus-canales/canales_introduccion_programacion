65f619e5e497ab4b6e1ac5bf744260ab18c793e8
Jesús Canales <177686251+jesus-canales@users.noreply.github.com>
2025-09-23 02:29:31 +0000

    Ejercicio de promedios

---
+++ 05_app.py
@@ -0,0 +1,7 @@
+# Obtener el promedio de un estudiante a partir de 3 notas
+
+# Inicio: Definir variables de las notas
+
+# Proceso: Definir el proceso de cálculo del promedio de notas
+
+# Salida: Mostrar el resultado del promedio
