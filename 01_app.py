641d84025540b2654aaf6184616d04e2d7c5e28e
Jesús Canales <177686251+jesus-canales@users.noreply.github.com>
2025-09-23 01:10:53 +0000

    Tarea

--- 01_app.py
+++ 01_app.py
@@ -5,3 +5,9 @@ print(4 + 5)
 
 # Resta
 print(100 - 45)
+
+# Multiplcación
+print(90 * 24)
+
+# División
+print(3000 / 13)
---
+++ 02_app.py
@@ -0,0 +1,16 @@
+# Elabora las siguientes operaciones
+
+# Sumar 144 más 255
+print(144 + 255)
+
+# Obtener 3 al cuadrado
+# print(3 ** ___) 
+
+# Calcular e imprimir 9 multiplicado por 2
+# print(____ ___ ____)
+
+# Imprimir el resultado de dividir 120 entre 2
+# _____( ___ _ __)
+
+
+
