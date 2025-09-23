284dd21139da02b6dc8308cb11cf20bf98cc7b2a
Jesús Canales <177686251+jesus-canales@users.noreply.github.com>
2025-09-23 01:25:23 +0000

    Empezamos con variables

---
+++ 03_app.py
@@ -0,0 +1,13 @@
+# Variables
+
+# Las variables son palabras específicas que utilizamos para asignar y almacenar valores específicos que podemos recuperar en cualquier momento.
+
+# Variable nombre
+nombre = "Jesús"
+telefono = 997654111
+name = "Alberto"
+
+# Visualizando el contenido de las variables
+print(nombre)
+print(telefono)
+print(name)
