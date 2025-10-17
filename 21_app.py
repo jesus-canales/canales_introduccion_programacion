7b2d9870ea6f2ac18ada5ff7959ea6baf3bd6c4c
Jesús Canales <177686251+jesus-canales@users.noreply.github.com>
2025-10-17 03:23:47 +0000

    Propuesta ejercicio 3

---
+++ 21_app.py
@@ -0,0 +1,8 @@
+# Ejercicio 3 - Funciones
+
+"""
+Calcula el precio final de un producto aplicando un descuento.
+El usuario debe ingresar un monto y un valor de porcentaje de descuento.
+Por ejemplo: Un producto cuesta 2500 y el porcentaje de descuento es 10%
+¿Cuanto es el monto a pagar?
+"""
