eb8bbc6a6a054eeb34e5892b56b072344387451a
Jesús Canales <177686251+jesus-canales@users.noreply.github.com>
2025-10-24 03:20:03 +0000

    Creamos nuestra primera lista

---
+++ 23_app.py
@@ -0,0 +1,10 @@
+# Listas
+
+# Creamos nuestra primera lista
+diasSemana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes"]
+
+# Imprimir la lista de elementos
+print(diasSemana)
+
+# Imprimir el tercer elemento
+print(diasSemana[2]) # posición de indexación
