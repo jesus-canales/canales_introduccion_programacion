070711e4be4f5a38d99fb22c1b3efd685e1ac648
Jesús Canales <177686251+jesus-canales@users.noreply.github.com>
2025-10-24 03:29:23 +0000

    Lista de número y posiciones inversas

---
+++ 24_app.py
@@ -0,0 +1,13 @@
+# Listas
+
+# Listado de 6 notas de un estudiante
+notas = [10, 17, 5, 20, 13, 15]
+
+# Imprimir listado de notas
+print(notas)
+
+# Imprimir la última nota
+print(notas[-1])
+
+# Imprimir la penúltima nota
+print(notas[-2])
