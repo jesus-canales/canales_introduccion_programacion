f80e33c6dd19653ed437b6981ffef3fedc2a9435
Jesús Canales <177686251+jesus-canales@users.noreply.github.com>
2025-10-24 03:12:04 +0000

    Impresion de boleta

--- 22_app.py
+++ 22_app.py
@@ -2,7 +2,7 @@ def obtenerDatosEmpleado():
     # Recibimos los datos del usuario
     nombre = input("Ingresa el nombre del colaborador: ");
     salarioBruto = input(f"Ingresa el salario bruto de {nombre} en S/. ");
-    porcentajeDescuento = input("Ingresa el porcentaje de descuento en %");
+    porcentajeDescuento = input("Ingresa el porcentaje de descuento en % ");
 
     # Convertir los datos a float para los cálculos
     salarioBrutoValor = float(salarioBruto)
@@ -19,3 +19,12 @@ def imprimirBoleta(nombreEmpleado, salarioBrutoEmpleado, porcentajeDescuentoEmpl
     print(f"Monto Descuento S/. {montoDescuento:.2f}")
     print(f"Salario Líquido S/. {salarioLiquido:.2f}")
     print("==================================")
+
+# Flujo del programa
+print("Sistema de Generación de Salario 2025")
+
+# Invocamos a la función obtenerDatosEmpleado
+nombre, salario, descuento = obtenerDatosEmpleado();
+
+# Generar la impresión de boleta
+imprimirBoleta(nombre, salario, descuento)
