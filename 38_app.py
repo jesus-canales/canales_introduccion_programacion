34d95d227995a902ccdfa128c37e93be2be6eb66
Jesús Canales <177686251+jesus-canales@users.noreply.github.com>
2025-10-28 02:43:24 +0000

    Trabajando con listas dentro de diccionarios

---
+++ 38_app.py
@@ -0,0 +1,7 @@
+# Diccionarios
+
+# Combinando elementos de diccionario
+agenda = {"Jesús":[45, "Lima"], "Luis":[35, "Trujillo"], "Claudia":[28, "Piura"]}
+
+# Imprimir los elementos del diccionario
+print(agenda)
