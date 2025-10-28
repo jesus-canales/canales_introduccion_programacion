8238938e9f3f598bb257f4287f0488d8ec143173
Jesús Canales <177686251+jesus-canales@users.noreply.github.com>
2025-10-28 02:09:59 +0000

    Gestionando elementos de tuplas

---
+++ 35_app.py
@@ -0,0 +1,13 @@
+# Tuplas
+
+# Meses del año
+tupla = ("enero", "febrero", "marzo")
+
+# Números enteros
+numeros = (1, 2, 3, 4, 5)
+
+# Unión de Tuplas
+nuevaTupla = tupla + numeros
+
+# Mostrar unión de tuplas
+print(nuevaTupla)
