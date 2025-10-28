ce204b8d8c1ed5e017e074f8a0fcd4f6aaef9408
Jesús Canales <177686251+jesus-canales@users.noreply.github.com>
2025-10-28 00:57:14 +0000

    Buscando elementos dentro de una lista

---
+++ 29_app.py
@@ -0,0 +1,14 @@
+# Listas
+
+# Tenemos áreas de una organización
+areas = ["Administración", "Recursos Humanos", "Contabilidad", "Sistemas"]
+
+# Buscar elementos dentro de lista
+print("Recursos Humanos" in areas)
+
+# Buscar elementos dentro de lista
+print("Recursos humanos" in areas)
+
+# Buscar contabilidad
+
+# Buscar Sistemas
