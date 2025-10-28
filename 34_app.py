8332fbad41aade06aafaf889a2e2bd1c327c7589
Jesús Canales <177686251+jesus-canales@users.noreply.github.com>
2025-10-28 02:06:34 +0000

    Recorrido por elementos de tupla

---
+++ 34_app.py
@@ -0,0 +1,13 @@
+# Tupla
+
+# Crear tupla con diversos tipos de elementos
+tupla = (4, 20, "Luis", 15.85, ["manzana", "fresa", "naranjas"], 10)
+
+# Imprimir elementos de tupla
+print(tupla)
+
+# Recorrer por los elementos de la tupla de izquierda a derecha
+print(tupla[4])
+
+# Recorrer por los elementos de la tupla de derecha a izquierda
+print(tupla[-2])
