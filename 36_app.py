cde4f1b2fe9c7b8bad33e9a32aa422044d643aa4
Jesús Canales <177686251+jesus-canales@users.noreply.github.com>
2025-10-28 02:24:01 +0000

    Empezamos a trabajar con diccionarios

---
+++ 36_app.py
@@ -0,0 +1,14 @@
+# Diccionarios
+
+# Crear un diccionario
+diccionario = {"azul":"blue", "rojo":"red", "verde":"green", "amarillo":"yellow"}
+
+# Imprimir los datos de un diccionario
+print(diccionario)
+
+# Obtener el valor de la clave azul
+print(diccionario["azul"])
+
+# Obtener el valor de la clave amarillo
+
+# Obtener el valor de la clave rojo
